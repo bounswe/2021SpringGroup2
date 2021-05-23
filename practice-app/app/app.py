3033e6fd1f12be51578549ce1fc1a762c24d327d
dogukanakar <dogukanakarc@gmail.com>
2021-05-23 16:14:56 +0300

    events list and getSpectators API added

--- practice-app/app/app.py
+++ practice-app/app/app.py
@@ -1,11 +1,38 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
+events = [
+    {
+        "eventId": 1,
+        "owner": "emre_gundogu",
+        "title":  "Tennis Game for Everyone",
+        "content": "People who are looking for a tennis court can apply this event. Our court is for you if you are seeking for a clean and well-lit court.",
+        "location": "Bebek Tennis Club",
+        "date": "01.05.2021",
+        "hours": "15.00",
+        "sport": "Tennis",
+        "ageGroup": "Any age",
+        "skillLevel": "Any level",
+        "playerCapacity": "2",
+        "spectatorCapacity": "8",
+        "spectators": [{"username":"zulfikar_tunc"}],
+        "players": [{"username":"zulfikar_tunc"}]
+    }
+]
 
 @app.route('/api/v1.0/', methods=['GET'])
 def index():
     return jsonify({'message': 'hello world'}), 200  # mock message as an example
+
+
+
+@app.route('/api/v1.0/spectators/<int:event_id>', methods=['GET'])
+def getSpectators(event_id):
+    event = [event for event in events if event['eventId'] == event_id]
+    if len(event) == 0:
+        abort(404)
+    return jsonify({'spectators': event[0]["spectators"]})
 
 
 if __name__ == '__main__':
