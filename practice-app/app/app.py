e19b38c3f9f7558c39a419a314b614b079539e9d
ege-kaya <egecky@gmail.com>
2021-05-26 01:18:28 +0300

    Added user list with a dummy user and applyAsPlayer API method

--- practice-app/app/app.py
+++ practice-app/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, Response
 
 app = Flask(__name__)
 
@@ -21,6 +21,22 @@ events = [
     }
 ]
 
+users = [
+    {
+        "userId": 1,
+        "nickname": "emre_gundogdu",
+        "firstName": "Emre",
+        "lastName": "Gundogdu",
+        "biography": "Hello, I am a 28 year-old football fan",
+        "age": "28",
+        "avatar": "url_to_image",
+        "location": "Istanbul",
+        "favSports": ["football", "basketball"],
+        "badges": ["good_player", "serious"],
+        "privacy": "public",
+    }
+]
+
 @app.route('/api/v1.0/', methods=['GET'])
 def index():
     return jsonify({'message': 'hello world'}), 200  # mock message as an example
@@ -33,6 +49,16 @@ def getSpectators(event_id):
     if len(event) == 0:
         abort(404)
     return jsonify({'spectators': event[0]["spectators"]})
+
+@app.route('/api/v1.0/events/<int:event_id>/players/<int:user_id>', methods=['POST'])
+def applyAsPlayer(event_id, user_id):
+    event = [event for event in events if event['eventId'] == event_id]
+    user = [user for user in users if user['userId'] == user_id]
+    if len(event) == 0:
+        abort(404)
+    event[0]["players"].append(user[0]['nickname'])
+    response = Response("%s applied to event %s" % (user[0]["nickname"], event[0]["title"]), 201, mimetype='application/json')
+    return response
 
 
 if __name__ == '__main__':
