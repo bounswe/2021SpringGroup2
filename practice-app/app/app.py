aadccbfb41fdbf16911f6500160cf8c330a07b24
Berkay Döner <berkay.doner@boun.edu.tr>
2021-06-01 12:48:09 +0300

    post and event post table initialization is added

--- practice-app/app/app.py
+++ practice-app/app/app.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer, BigInteger, Boolean
+from sqlalchemy import Column, String, Integer, BigInteger, Boolean, MetaData, Date, Time, DateTime, Text
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import ENUM, NUMRANGE, INT4RANGE, ARRAY
 from sqlalchemy.orm import sessionmaker
 import requests
 from flask import Flask, jsonify, abort, request
@@ -163,6 +164,29 @@ class User(base):
     badge_2 = Column(String)
     badge_3 = Column(String)
     privacy = Column(Boolean)
+
+class post(base):
+    __abstract__ = True
+    __tablename__ = "post"
+    postID = Column(Integer,primary_key=True)
+    ownerID = Column(Integer,nullable=False)
+    content = Column(Text)
+    title = Column(String(100),nullable=False)
+    creationDate = Column(DateTime,nullable=False)
+    location = Column(String(200))
+
+class eventpost(post):
+    __tablename__ = "eventpost"
+    eventDate = Column(Date,nullable=False)
+    eventHours = Column(Time,nullable=False)
+    eventSport = Column(String(30),nullable=False)
+    eventAgeGroup = Column(INT4RANGE)
+    eventPlayerCapacity = Column(Integer,nullable=False)
+    eventSpectatorCapacity = Column(Integer)
+    eventPlayers = Column(ARRAY(Integer))
+    eventSpectators = Column(ARRAY(Integer))
+    eventSkillLevel = Column(ENUM('Beginner', 'Preintermediate', 'Intermediate','Advanced','Expert', name='skill'))
+    eventCoordinates = Column(NUMRANGE, nullable=False)
 
 Session = sessionmaker(db)
 session = Session()
