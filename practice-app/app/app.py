aa074428803dd2f3201d97004f97bfc07341734c
berkaydoner <berkaydoner13@gmail.com>
2021-06-02 22:10:14 +0300

    Refs #58 - separated database initalization code

--- practice-app/app/app.py
+++ practice-app/app/app.py
@@ -1,8 +1,3 @@
-from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer, BigInteger, Boolean, MetaData, Date, Time, DateTime, Text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.dialects.postgresql import ENUM, NUMRANGE, INT4RANGE, ARRAY
-from sqlalchemy.orm import sessionmaker
 import requests
 from flask import Flask, jsonify, abort, request
 import urllib
@@ -142,56 +137,6 @@ def get_event(event_id):
     return jsonify({'events': event,
                     'covid_risk_status': covid_risk,
                     'current_cases': covid_data[-1]['Cases']})
-
-db = create_engine('postgresql://practice_user:-#My6o0dPa33W0rd#-@localhost:5432/practiceapp_db')
-base = declarative_base()
-
-class User(base):
-    __tablename__ = 'users'
-
-    user_id = Column(BigInteger, primary_key=True)
-    nickname = Column(String)
-    first_name = Column(String)
-    last_name = Column(String)
-    biography = Column(String)
-    birth_year = Column(Integer)
-    avatar = Column(String)
-    location = Column(String)
-    fav_sport_1 = Column(String)
-    fav_sport_2 = Column(String)
-    fav_sport_3 = Column(String)
-    badge_1 = Column(String)
-    badge_2 = Column(String)
-    badge_3 = Column(String)
-    privacy = Column(Boolean)
-
-class post(base):
-    __abstract__ = True
-    __tablename__ = "post"
-    postID = Column(Integer,primary_key=True)
-    ownerID = Column(Integer,nullable=False)
-    content = Column(Text)
-    title = Column(String(100),nullable=False)
-    creationDate = Column(DateTime,nullable=False)
-    location = Column(String(200))
-
-class eventpost(post):
-    __tablename__ = "eventpost"
-    eventDate = Column(Date,nullable=False)
-    eventHours = Column(Time,nullable=False)
-    eventSport = Column(String(30),nullable=False)
-    eventAgeGroup = Column(INT4RANGE)
-    eventPlayerCapacity = Column(Integer,nullable=False)
-    eventSpectatorCapacity = Column(Integer)
-    eventPlayers = Column(ARRAY(Integer))
-    eventSpectators = Column(ARRAY(Integer))
-    eventSkillLevel = Column(ENUM('Beginner', 'Preintermediate', 'Intermediate','Advanced','Expert', name='skill'))
-    eventCoordinates = Column(NUMRANGE, nullable=False)
-
-Session = sessionmaker(db)
-session = Session()
-
-base.metadata.create_all(db)
 
 @app.route('/api/v1.0/', methods=['GET'])
 def index():
