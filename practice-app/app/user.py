b81e776e65b20f2acdd5edecbed42cc05b527ad4
ŞefikaAkman <sefikaakman4@hotmail.com>
2021-06-06 17:29:28 +0300

    added post_user

--- practice-app/app/user.py
+++ practice-app/app/user.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify, abort, request
 import urllib
 from datetime import datetime, timedelta
 from math import cos, asin, sqrt, pi
+from dbinit import User
+
 
 app = Flask(__name__)
 API_KEY = "Google API Key"
@@ -87,6 +89,31 @@ headers = {
     "x-rapidapi-key": "c4ab16012amsh73b5a257264eb3dp11ade4jsnb69ec0b79098",
     "x-rapidapi-host" :"google-search3.p.rapidapi.com"
 }
+
+
+
+@app.route('/api/v1.0/users', methods=['POST'])
+def post_user(user_id):
+    body = request.json
+    user_id = body["userId"]
+    user = [user for user in users if user["userId"] == user_id]
+    if len(user) == 0:
+        response = requests.get("https://randomapi.com/api/9fekfc0v?key=2UX0-XIT1-7DYM-WDBC")
+        json_data = response.json()["results"]
+        new_user = User(user_id=user_id,
+                        nickname=json_data[0]["nickname"],
+                        first_name=json_data[0]["firstName"],
+                        last_name=json_data[0]["lastName"],
+                        biography=json_data[0]["biography"],
+                        birth_year=json_data[0]["birth_year"],
+                        avatar=json_data[0]["avatar"],
+                        location=json_data[0]["location"],
+                        privacy=False if json_data[0]["privacy"] == 'private' else True)
+        users.append(new_user)
+        return jsonify({"applicantId": user_id,
+                    "applicantNickname": user[0]["nickname"],
+                    "applicationServerTime": datetime.now().strftime("%d/%m/%Y %H:%M:%S")}), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
