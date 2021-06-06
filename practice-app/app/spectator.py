a0eafbf4886b4fa6479661a6beaeb8b00662bed9
dogukanakar <dogukanakarc@gmail.com>
2021-06-06 17:49:36 +0300

    get_spectator updated using the Eventpost db table

--- practice-app/app/spectator.py
+++ practice-app/app/spectator.py
@@ -3,6 +3,7 @@ from flask import Flask, Blueprint, jsonify, abort, request
 import urllib
 from datetime import datetime, timedelta
 from math import cos, asin, sqrt, pi
+from dbinit import eventpost, session
 
 spectator_api = Blueprint('spectator_api', __name__)
 API_KEY = "Google API Key"
@@ -89,10 +90,10 @@ headers = {
 }
 @app.route('/api/v1.0/spectators/<int:event_id>', methods=['GET'])
 def get_spectators(event_id):
-    event = [event for event in events if event['eventId'] == event_id]
-    if len(event) == 0:
+    event = session.query(eventpost).get(event_id)
+    if event == null:
         abort(404)
-    return jsonify({'spectators': event[0]["spectators"]})
+    return jsonify({'spectators': event.eventSpectators})
 
 @app.route('/api/v1.0/events/<int:event_id>/spectators', methods=['POST'])
 def postSpectator(event_id):
