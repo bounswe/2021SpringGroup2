0291c7350c3072c500a99d344ea69f1c42f486e6
dogukanakar <dogukanakarc@gmail.com>
2021-06-08 01:12:49 +0300

    post and get answer methods added

--- practice-app/app/answer.py
+++ practice-app/app/answer.py
@@ -2,99 +2,33 @@ import requests
 from flask import Flask, Blueprint, jsonify, abort, request
 import urllib
 from datetime import datetime, timedelta
-from .dbinit import Answer, session
+from .dbinit import Answer, session, Eventpost, Comment, Blocking, User
+import datetime
 
 answer_api = Blueprint('answer_api', __name__)
-API_KEY = "Google API Key"
-
-events = [
-    {
-        "eventId": 1,
-        "owner": "emre_gundogu",
-        "title":  "Tennis Game for Everyone",
-        "content": "People who are looking for a tennis court can apply this event. Our court is for you if you are seeking for a clean and well-lit court.",
-        "location": "Etiler Tennis Club",
-        "coordinates": (41.0869173, 29.0321301),
-        "date": "01.05.2021",
-        "hours": "15.00",
-        "sport": "Tennis",
-        "ageGroup": "Any age",
-        "skillLevel": "Any level",
-        "playerCapacity": "2",
-        "spectatorCapacity": "8",
-        "spectators": [{"username":"zulfikar_tunc"}],
-        "players": [{"username":"zulfikar_tunc"}]
-    }
-]
-
-users = [
-    {
-        "userId": 1,
-        "nickname": "emre_gundogu",
-        "first_name": "Emre",
-        "last_name": "Gundogu",
-        "biography": "Hello, I am a 28 year-old football fan",
-        "birth_year": "28",
-        "avatar": "url_to_image",
-        "location": "Istanbul",
-        "fav_sports": ["football", "basketball"],
-        "badges": ["good_player", "serious"],
-        "privacy": "public",
-    }
-]
-
-equipmentPost = [
-	{
-		"postId": 1,
-		"ownerId": 1,
-		"content": "A nice ball",
-		"title": "Well-conditioned ball",
-		"creationDate": "29.05.2021",
-		"lastUpdateDate": "29.05.2021",
-		"numberOfClicks": 1,
-		"location": "İstanbul",
-		"equipmentType": "Ball",
-		"websiteName": "ismycomputeron",
-		"link": "www.ismycomputeron.com"
-	}
-]
-equipments2 = [
-    {
-        "equipmentId": 1,
-        "ownerId": 1,
-        "title" : "Tennis shoes for sale!",
-        "content" : "I have a pair of shoes in good condition that i want to sell.",
-        "website name" : "letgo",
-        "link" : "letgo.com/245323",
-        "equipmentType": "shoes",
-        "location": "Istanbul",
-        "sportType": "Tennis"
-
-    },
-    {
-        "equipmentId": 2,
-        "ownerId": 1,
-        "title" : "Tennis rackets for sale!",
-        "content" : "I have a pair of shoes in good condition that i want to sell.",
-        "website name" : "letgo",
-        "link" : "letgo.com/245323",
-        "equipmentType": "racket",
-        "location": "Ankara",
-        "sportType": "Tennis"
-    }
-]
-headers = {
-    "x-rapidapi-key": "c4ab16012amsh73b5a257264eb3dp11ade4jsnb69ec0b79098",
-    "x-rapidapi-host" :"google-search3.p.rapidapi.com"
-}
-
 
 @answer_api.route('/api/v1.0/<int:post_id>/comments/<int:comment_id>/answers', methods=['GET'])
 def get_comment_answers(post_id, comment_id):
-    session.query(Answer)
-    if len(event) == 0:
-        abort(404)
-    return jsonify({'spectators': event[0]["spectators"]})
+    post = session.query(Eventpost).filter(Eventpost.postID==post_id).first()
+    comment = session.query(Comment).filter(Comment.commentID==comment_id).first()
+    if post is None or comment is None:
+        return abort(404)
+    answers = session.query(Answer).filter(Answer.commentId==comment_id).all()
+    return jsonify({"post": post, "comment": comment, "answers": list(answers)})
+
+@answer_api.route('/api/v1.0/<int:post_id>/comments/<int:comment_id>/answers', methods=['POST'])
+def post_comment_answers(post_id, comment_id):
+    if "answer" not in request.json or "owner_id" not in request.json:
+        return abort(400)
+    post = session.query(Eventpost).filter(Eventpost.postID==post_id).first()
+    comment = session.query(Comment).filter(Comment.commentID==comment_id).first()
+    user = session.query(User).filter(User.user_id==request.json["owner_id"]).first()
+    if post is None or comment is None or user is None:
+        return abort(404)
+    new_answer = Answer(commentId=comment_id, answer=request.json["answer"], answerDate=datetime.datetime.utcnow(), ownerID=request.json["owner_id"])
+    session.add(new_answer)
+    session.commit()
+    return jsonify(new_answer)
 
 
 if __name__ == '__main__':
