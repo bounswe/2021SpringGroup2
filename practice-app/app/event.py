6745c3c876d904ff2a6eb6fe93cd07279eae83b2
berkaydoner <berkaydoner13@gmail.com>
2021-06-09 13:33:40 +0300

    Fixed the capital letter issue of table names. Also removed the session object, it is imported.

--- practice-app/app/event.py
+++ practice-app/app/event.py
@@ -10,96 +10,12 @@ from sqlalchemy.sql.functions import func
 from sqlalchemy import or_, and_
 from datetime import datetime, time, date
 from math import cos, asin, sqrt, pi
-from .dbinit import db, User, Eventpost
+from .dbinit import db, User, Eventpost, session
 from sqlalchemy.orm import sessionmaker
-
-Session = sessionmaker(db)
-session = Session()
 
 event_api = Blueprint('event_api', __name__)
 API_KEY = "Google API Key"
 
-db = create_engine('postgresql://practice_user:-#My6o0dPa33W0rd#-@localhost:5432/practiceapp_db')
-
-Session = sessionmaker(db)  
-session = Session()
-
-events = [
-    {
-        "eventId": 1,
-        "owner": "emre_gundogu",
-        "title":  "Tennis Game for Everyone",
-        "content": "People who are looking for a tennis court can apply this event. Our court is for you if you are seeking for a clean and well-lit court.",
-        "location": "Etiler Tennis Club",
-        "coordinates": (41.0869173, 29.0321301),
-        "date": "01.05.2021",
-        "hours": "15.00",
-        "sport": "Tennis",
-        "ageGroup": "Any age",
-        "skillLevel": "Any level",
-        "playerCapacity": "2",
-        "spectatorCapacity": "8",
-        "spectators": [{"username":"zulfikar_tunc"}],
-        "players": [{"username":"zulfikar_tunc"}]
-    }
-]
-
-users = [
-    {
-        "userId": 1,
-        "nickname": "emre_gundogu",
-        "first_name": "Emre",
-        "last_name": "Gundogu",
-        "biography": "Hello, I am a 28 year-old football fan",
-        "birth_year": "28",
-        "avatar": "url_to_image",
-        "location": "Istanbul",
-        "fav_sports": ["football", "basketball"],
-        "badges": ["good_player", "serious"],
-        "privacy": "public",
-    }
-]
-
-equipmentPost = [
-	{
-		"postId": 1,
-		"ownerId": 1,
-		"content": "A nice ball",
-		"title": "Well-conditioned ball",
-		"creationDate": "29.05.2021",
-		"lastUpdateDate": "29.05.2021",
-		"numberOfClicks": 1,
-		"location": "İstanbul",
-		"equipmentType": "Ball",
-		"websiteName": "ismycomputeron",
-		"link": "www.ismycomputeron.com"
-	}
-]
-equipments2 = [
-    {
-        "equipmentId": 1,
-        "ownerId": 1,
-        "title" : "Tennis shoes for sale!",
-        "content" : "I have a pair of shoes in good condition that i want to sell.",
-        "website name" : "letgo",
-        "link" : "letgo.com/245323",
-        "equipmentType": "shoes",
-        "location": "Istanbul",
-        "sportType": "Tennis"
-
-    },
-    {
-        "equipmentId": 2,
-        "ownerId": 1,
-        "title" : "Tennis rackets for sale!",
-        "content" : "I have a pair of shoes in good condition that i want to sell.",
-        "website name" : "letgo",
-        "link" : "letgo.com/245323",
-        "equipmentType": "racket",
-        "location": "Ankara",
-        "sportType": "Tennis"
-    }
-]
 headers = {
     "x-rapidapi-key": "c4ab16012amsh73b5a257264eb3dp11ade4jsnb69ec0b79098",
     "x-rapidapi-host" :"google-search3.p.rapidapi.com"
@@ -173,7 +89,7 @@ def getNearbyEvents():
         return make_response(jsonify({'error': 'A numeric value must be given as the radius parameter.'}), 400)
     if useIP=="false" and address is None: ## if useIP is false, an address should be provided
         return make_response(jsonify({'error': 'Either an address should be given or the IP address must be used.'}), 400)
-    query = session.query(eventpost) ##initialize query
+    query = session.query(Eventpost) ##initialize query
     for argument in request.args:
         if argument == 'address' or argument == 'radius' or argument =='ip':
             continue
@@ -181,34 +97,34 @@ def getNearbyEvents():
             if not request.args["empty"] in ["true","false"]: ## must be boolean
                 return make_response(jsonify({'error': 'Empty argument must be true or false.'}), 400)
             if request.args["empty"]=="true":
-                query = query.filter(func.cardinality(eventpost.eventPlayers)<eventpost.eventPlayerCapacity) ## if given, filter 
+                query = query.filter(func.cardinality(Eventpost.eventPlayers)<Eventpost.eventPlayerCapacity) ## if given, filter 
         elif argument == "sport": ## sport type to filter the events
             if not request.args["sport"].isalpha(): ## must be a string
                 return make_response(jsonify({'error': 'Sport type must consist of alphabetic characters only.'}), 400)
-            query = query.filter(eventpost.eventSport==str(request.args["sport"])) ## return only the events of the given sport 
+            query = query.filter(Eventpost.eventSport==str(request.args["sport"])) ## return only the events of the given sport 
         elif argument == "skillLevel": ## skill level of events
             if not request.args["skillLevel"] in ["Beginner","Pre-intermediate","Intermediate","Advanced","Expert"]: ## must be enum of 5 values
                 return make_response(jsonify({'error': 'Skill level must be one of the following: Beginner,Pre-intermediate,Intermediate,Advanced,Expert.'}), 400)
-            query = query.filter(eventpost.eventSkillLevel==str(request.args["skillLevel"]))
+            query = query.filter(Eventpost.eventSkillLevel==str(request.args["skillLevel"]))
         elif argument == "search": ## keyword to search in the title and description of events
             searchContent = request.args["search"]
             if not searchContent.isalpha(): ## must be string
                 return make_response(jsonify({'error': 'Search content must consist of alphabetic characters only.'}), 400)
-            query = query.filter(or_(eventpost.title.ilike('%'+str(searchContent)+'%'), eventpost.content.ilike('%'+str(searchContent)+'%')))
+            query = query.filter(or_(Eventpost.title.ilike('%'+str(searchContent)+'%'), Eventpost.content.ilike('%'+str(searchContent)+'%')))
         elif argument == "ageGroup": ## an integer tuple that represents the age range
             ageRange = request.args["ageGroup"]
             if re.search("^\d+,\d+$",ageRange) is None: ## must be tuple of ints
                 return make_response(jsonify({'error': 'Age range is not in valid format.'}), 400)
             ageRange = tuple(ageRange.split(","))
-            query = query.filter(and_(int(ageRange[0]) <= func.lower(eventpost.eventAgeGroup),int(ageRange[1]) >= func.upper(eventpost.eventAgeGroup)))
+            query = query.filter(and_(int(ageRange[0]) <= func.lower(Eventpost.eventAgeGroup),int(ageRange[1]) >= func.upper(Eventpost.eventAgeGroup)))
         elif argument == "dateBegin": ## a datetime object for the lower bound of the dates of events
             try: ## if dateBegin is given, dateEnd must be also given. Only intervals are supported for now.
                 datetime_begin_object = datetime.strptime(request.args["dateBegin"],"%d/%m/%Y %H:%M:%S") ## format should be DD/MM/YYYY HH:MM:SS
                 datetime_end_object = datetime.strptime(request.args["dateEnd"], "%d/%m/%Y %H:%M:%S")
             except ValueError:
                 return make_response(jsonify({'error': 'Date and time must be in format DD/MM/YYYY HH:MM:SS'}), 400)
-            query = query.filter(and_(func.date(datetime_begin_object.date())<=func.date(eventpost.eventDate), func.date(datetime_end_object.date())>=func.date(eventpost.eventDate))).\
-                filter(and_(datetime_begin_object.time()<=eventpost.eventHours, datetime_end_object.time()>=eventpost.eventHours))
+            query = query.filter(and_(func.date(datetime_begin_object.date())<=func.date(Eventpost.eventDate), func.date(datetime_end_object.date())>=func.date(Eventpost.eventDate))).\
+                filter(and_(datetime_begin_object.time()<=Eventpost.eventHours, datetime_end_object.time()>=Eventpost.eventHours))
     if useIP=="false": ## if useIP is false, find coordinates using Geocoding API
         getParams = {'address':' '.join(str(address).split()),'key':API_KEY} ## parameters are the address and the key
         addressData = requests.get("https://maps.googleapis.com/maps/api/geocode/json",params=getParams).json()
@@ -224,20 +140,20 @@ def getNearbyEvents():
         lat = ipdata["location"]["lat"]
         lng = ipdata["location"]["lng"]
 
-    nearbyEvents = query.filter(func.haversineDistance(lat,lng,eventpost.eventLatitude,eventpost.eventLongitude)<=radius)
+    nearbyEvents = query.filter(func.haversineDistance(lat,lng,Eventpost.eventLatitude,Eventpost.eventLongitude)<=radius)
     ## filter the events so that only the events within the given radius of the calculated coordinates are displayed. Haversine Distance is used
     if not request.args.get("orderby") is None: ## if orderby parameter is given
-        if not request.args["orderby"] in eventpost.__table__.columns.keys(): ## it should be a column value
+        if not request.args["orderby"] in Eventpost.__table__.columns.keys(): ## it should be a column value
             return make_response(jsonify({'error': 'Order criterion is not a valid column name.'}), 400)
         if not request.args.get("order") is None: ## if order direction is given
             if not request.args["order"] in ["asc","desc"]: ## should be either asc or desc
                 return make_response(jsonify({'error': 'Order direction must be either asc or desc'}), 400)
             if request.args["order"]=="asc": 
-                nearbyEvents = nearbyEvents.order_by(getattr(eventpost,request.args["orderby"]).asc()) ## ascending order
+                nearbyEvents = nearbyEvents.order_by(getattr(Eventpost,request.args["orderby"]).asc()) ## ascending order
             else:
-                nearbyEvents = nearbyEvents.order_by(getattr(eventpost,request.args["orderby"]).desc())
+                nearbyEvents = nearbyEvents.order_by(getattr(Eventpost,request.args["orderby"]).desc())
         else:
-            nearbyEvents = nearbyEvents.order_by(getattr(eventpost,request.args["orderby"]).asc())
+            nearbyEvents = nearbyEvents.order_by(getattr(Eventpost,request.args["orderby"]).asc())
     eventList = [] 
     nearbyEvents = nearbyEvents.all() ## take the events satisfying the filters
     for i in nearbyEvents:
