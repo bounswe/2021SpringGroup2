22056f62da98689e1b9c8fc52eae5b8ced433ed9
Voursstrreds <56487501+Voursstrreds@users.noreply.github.com>
2021-06-10 18:53:35 +0300

    Unit test for Post Equipment is added.

--- practice-app/test/test.py
+++ practice-app/test/test.py
@@ -1,27 +1,51 @@
 import unittest
 
 from app.app import app
-from app.dbinit import db, User, base, session
+from app.dbinit import db, User, base, session, Equipmentpost
 from app import dbinit
 
-
-class SimpleTestCase(unittest.TestCase):
+class PostEquipmentTest(unittest.TestCase):
 
     def setUp(self):
-        self.client = app.test_client()
-        user = User(user_id=48, nickname='emre121', first_name='emre', last_name='guner')
+        user = User()
         session.add(user)
         session.commit()
 
-    def test_content(self):
-        resp = self.client.get('/api/v1.0/events/1')
+        self.client = app.test_client()
+
+    def test_equipment_valid(self):
+        params = {
+            "ownerID": 1,
+            "content": "nohut",
+            "title": "fsfsfs",
+            "location": "ooo",
+            "equipmentType": "pwp",
+            "websiteName": "ooeoo",
+            "link": "ooeoo.com"}
+
+        resp = self.client.post('/api/v1.0/equipments/', data=params)
 
         status = resp.status_code
 
-        self.assertEqual(404, status)
+        self.assertEqual(201, status)
+
+    def test_equipment_invalid(self):
+        params = {
+            "ownerID": 1,
+            "content": "nohut",
+            "title": "fsfsfs",
+            "location": "ooo",
+            "websiteName": "ooeoo",
+            "link": "ooeoo.com"}
+
+        resp = self.client.post('/api/v1.0/equipments/', data=params)
+
+        status = resp.status_code
+
+        self.assertEqual(400, status)
 
     def tearDown(self):
-        dbinit.session.close()
-        dbinit.base.metadata.drop_all(db)
+        session.close()
+        base.metadata.drop_all(db)
 
 
