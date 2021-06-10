913ef9b2e9c694d3a672e9ee21bde97f53b2a37b
ibrahim <ibrahimbayat1999@hotmail.com>
2021-06-10 12:07:51 +0300

    changed according to review

--- practice-app/test/test.py
+++ practice-app/test/test.py
@@ -17,3 +17,38 @@ class SimpleTestCase(unittest.TestCase):
         self.assertEqual(200, status)
         self.assertEqual('hello world', msg)
 
+
+class Testplayers(unittest.TestCase):
+    def test_is_proper_players_anoldplayer(self):
+        result=is_proper_players(50,10,"football",[["player1",61,["football","basketbol","f1"]], ["player2 ",35,["bowling","basketbol","football"]  ] ,["player3 ",45,["walking","running","football"] ]])
+        self.assertEqual(result,"There is an older player.")
+    def test_is_proper_players_ayoungplayer(self):
+        result=is_proper_players(50,10,"football",[["player1",9,["football","basketbol","f1"]], ["player2 ",35,["bowling","basketbol","football"]  ] ,["player3 ",45,["walking","running","football"] ]])
+        self.assertEqual(result,"There is a younger player.")
+    def test_is_proper_players_someplayersontlikethissport(self):
+        result=is_proper_players(50,10,"football",[["player1",44,["running","basketbol","f1"]], ["player2 ",35,["bowling","basketbol","football"]  ] ,["player3 ",45,["walking","running","football"] ]])
+        self.assertEqual(result,"Some players dont like this sport.")
+    def test_is_proper_players_allplayerareappropriate(self):
+        result=is_proper_players(50,10,"football",[["player1",12,["football","basketbol","f1"]], ["player2 ",35,["bowling","basketbol","football"]  ] ,["player3 ",45,["walking","running","football"] ]])
+        self.assertEqual(result,"All players satisfy the needs.")
+
+def is_proper_players(maxage,minage,sport,players):
+    number=0
+    print(players)
+    for a in range (len(players)):
+        if players[a][1]>maxage:
+            return "There is an older player."
+        if players[a][1]<minage:
+            return "There is a younger player."
+    for a in range (len(players)):
+        for b in range (len(players[a][2])):
+            
+            if(players[a][2][b]==sport):
+                
+                number=number+1
+    if(number==len(players)):
+            
+        return "All players satisfy the needs."
+    else:
+            
+        return "Some players dont like this sport."
