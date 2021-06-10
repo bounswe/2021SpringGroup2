b80c208f8af522f25593706a880b0489a482bec1
ŞefikaAkman <sefikaakman4@hotmail.com>
2021-06-10 09:56:54 +0300

    get_notification functon added

--- practice-app/app/notification.py
+++ practice-app/app/notification.py
@@ -3,89 +3,16 @@ from flask import Flask, Blueprint, jsonify, abort, request
 import urllib
 from datetime import datetime, timedelta
 from math import cos, asin, sqrt, pi
+from .dbinit import session, Notification
 
 notif_api = Blueprint('notif_api', __name__)
 API_KEY = "Google API Key"
 
-events = [
-    {
-        "eventId": 1,
-        "owner": "emre_gundogu",
-        "title":  "Tennis Game for Everyone",
-        "content": "People who are looking for a tennis court can apply this event. Our court is for you if you are seeking for a clean and well-lit court.",
-        "location": "Etiler Tennis Club",
-        "coordinates": (41.0869173, 29.0321301),
-        "date": "01.05.2021",
-        "hours": "15.00",
-        "sport": "Tennis",
-        "ageGroup": "Any age",
-        "skillLevel": "Any level",
-        "playerCapacity": "2",
-        "spectatorCapacity": "8",
-        "spectators": [{"username":"zulfikar_tunc"}],
-        "players": [{"username":"zulfikar_tunc"}]
-    }
-]
+@notif_api.route("/api/v1.0/notification",methods=["GET"])
+def get_notification(user_id):
+    notifications = session.query(Notification).filter(Notification.recipientID == user_id).all()
+    return jsonify(notifications), 200
 
-users = [
-    {
-        "userId": 1,
-        "nickname": "emre_gundogu",
-        "first_name": "Emre",
-        "last_name": "Gundogu",
-        "biography": "Hello, I am a 28 year-old football fan",
-        "birth_year": "28",
-        "avatar": "url_to_image",
-        "location": "Istanbul",
-        "fav_sports": ["football", "basketball"],
-        "badges": ["good_player", "serious"],
-        "privacy": "public",
-    }
-]
 
-equipmentPost = [
-	{
-		"postId": 1,
-		"ownerId": 1,
-		"content": "A nice ball",
-		"title": "Well-conditioned ball",
-		"creationDate": "29.05.2021",
-		"lastUpdateDate": "29.05.2021",
-		"numberOfClicks": 1,
-		"location": "İstanbul",
-		"equipmentType": "Ball",
-		"websiteName": "ismycomputeron",
-		"link": "www.ismycomputeron.com"
-	}
-]
-equipments2 = [
-    {
-        "equipmentId": 1,
-        "ownerId": 1,
-        "title" : "Tennis shoes for sale!",
-        "content" : "I have a pair of shoes in good condition that i want to sell.",
-        "website name" : "letgo",
-        "link" : "letgo.com/245323",
-        "equipmentType": "shoes",
-        "location": "Istanbul",
-        "sportType": "Tennis"
-
-    },
-    {
-        "equipmentId": 2,
-        "ownerId": 1,
-        "title" : "Tennis rackets for sale!",
-        "content" : "I have a pair of shoes in good condition that i want to sell.",
-        "website name" : "letgo",
-        "link" : "letgo.com/245323",
-        "equipmentType": "racket",
-        "location": "Ankara",
-        "sportType": "Tennis"
-    }
-]
-headers = {
-    "x-rapidapi-key": "c4ab16012amsh73b5a257264eb3dp11ade4jsnb69ec0b79098",
-    "x-rapidapi-host" :"google-search3.p.rapidapi.com"
-}
 
 
