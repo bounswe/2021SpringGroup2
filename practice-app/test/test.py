3a0b6b1762bf1069a4c36b45e903bd26d691e44a
Voursstrreds <56487501+Voursstrreds@users.noreply.github.com>
2021-06-10 18:52:35 +0300

    Unit test for GET User is added.

--- practice-app/test/test.py
+++ practice-app/test/test.py
@@ -4,24 +4,35 @@ from app.app import app
 from app.dbinit import db, User, base, session
 from app import dbinit
 
-
-class SimpleTestCase(unittest.TestCase):
+class GetUserTest(unittest.TestCase):
 
     def setUp(self):
-        self.client = app.test_client()
-        user = User(user_id=48, nickname='emre121', first_name='emre', last_name='guner')
-        session.add(user)
+        user1 = User()
+        user2 = User()
+        session.add_all([user1, user2])
         session.commit()
 
-    def test_content(self):
-        resp = self.client.get('/api/v1.0/events/1')
+        self.client = app.test_client()
 
+    def test_get_valid_single_user(self):
+
+        resp = self.client.get('/api/v1.0/users/1')
         status = resp.status_code
+        self.assertEqual(200, status)
 
+    def test_get_valid_all_users(self):
+
+        resp = self.client.get('/api/v1.0/users')
+        status = resp.status_code
+        self.assertEqual(200, status)
+
+    def test_get_invalid(self):
+
+        resp = self.client.get('/api/v1.0/users/493')
+        status = resp.status_code
         self.assertEqual(404, status)
 
     def tearDown(self):
-        dbinit.session.close()
-        dbinit.base.metadata.drop_all(db)
-
+        session.close()
+        base.metadata.drop_all(db)
 
