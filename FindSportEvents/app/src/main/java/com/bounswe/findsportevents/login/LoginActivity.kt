973725f0a1e3aceac267ebd6286da9332c7f3939
Omer Arslan <omerarslnn@gmail.com>
2021-11-09 14:06:22 +0300

    Add: Login activity input check

--- FindSportEvents/app/src/main/java/com/bounswe/findsportevents/login/LoginActivity.kt
+++ FindSportEvents/app/src/main/java/com/bounswe/findsportevents/login/LoginActivity.kt
@@ -2,6 +2,10 @@ package com.bounswe.findsportevents.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import com.bounswe.findsportevents.main.MainActivity
 import com.bounswe.findsportevents.databinding.ActivityLoginBinding
@@ -16,11 +20,50 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.etUsername.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                checkFields()
+            }
+
+        })
+
+        binding.etPassword.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                checkFields()
+            }
+
+        })
+
         binding.buttonLogin.setOnClickListener {
             startActivity<MainActivity> {
                 addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
                 MainActivity.addExtras(this, true)
             }
         }
+    }
+
+    private fun checkFields() {
+        binding.buttonLogin.isEnabled = binding.etUsername.text.toString().length >= 5 && binding.etPassword.text.toString().length >= 3
+                && isValidEmail(binding.etUsername.text.toString())
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 }
