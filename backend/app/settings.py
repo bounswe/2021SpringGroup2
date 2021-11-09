9de4154c5895a9fde984756b66ffa946a0b0252f
Bengisu Ozaydin <bengisuozaydin@gmail.com>
2021-11-09 14:39:46 +0300

    #144 - Removed sessions app because it is legacy.

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
