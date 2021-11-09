9919791a2d3f1ca5805f670bbaccf84a3f5618db
dogukanakar <dogukanakarc@gmail.com>
2021-11-09 22:10:49 +0300

    fixed the error about usetheme hook

--- frontend/src/Views/Shared/Layout.js
+++ frontend/src/Views/Shared/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Outlet, Link, useLocation} from 'react-router-dom'
+import {Outlet} from 'react-router-dom'
 import Header from './Header'
 import {createTheme, ThemeProvider} from "@mui/material/styles";
 
 
-const theme = createTheme();
 
 const Layout = () => {
+    const theme = createTheme();
 
     return (
         <React.Fragment>
