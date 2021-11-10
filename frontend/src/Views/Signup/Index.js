9bc5f741e842fa5109828eafe09804eb73578252
dogukanakar <dogukanakarc@gmail.com>
2021-11-10 21:03:52 +0300

    #155 fix for minimum password length (3 to 8)

--- frontend/src/Views/Signup/Index.js
+++ frontend/src/Views/Signup/Index.js
@@ -59,12 +59,12 @@ const schema = Joi.object({
         .required(),
 
     password: Joi.string()
-        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+        .pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')),
 
     repeat_password: Joi.ref('password'),
 
     email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', "edu"] } })
+        .email({ minDomainSegments: 2 })
 })
     .with('password', 'repeat_password');
 
