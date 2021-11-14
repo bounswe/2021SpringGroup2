df3b85509749fe6039e8cb46d80b51f58a421270
ege-kaya <egecky@gmail.com>
2021-11-14 16:28:40 +0300

    added url pathing, fixed file hierarchy, some minor additions

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('authentication.urls')),
+    path('api/posts/', include('eventposts.urls')),
 ]
--- backend/eventposts/urls.py
+++ backend/eventposts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib import admin
+
 urlpatterns=[
     
     path('<int:id>/', views.EventPostView.as_view(), name="EventPost"),
