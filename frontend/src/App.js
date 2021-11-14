44344f9c923aab5979632736bfd0438138dacb4b
berkaydoner <52797716+berkaydoner@users.noreply.github.com>
2021-11-14 17:39:51 +0300

    Fixed routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,21 @@ import {
 
 import Home from './Views/Home/Index'
 import Login from './Views/Login/Index'
-import Layout from './Views/Shared/Layout'
-import Signup from './Views/Signup/Index'
-
-
+import Header from './Views/Shared/Header'
+import CreateEventPage from "./Views/Create Event/CreateEventPage";
+import ResetPasswordPage from "./Views/Login/ForgotPassword";
 
 const App = () => {
     console.log("sa")
     return (
         <React.Fragment>
             <Routes>
-                <Route path="/" element={<Layout />}>
+                <Route path="/" element={<Header />}>
                     <Route index element={<Home/>}/>
                     <Route path='login' element={<Login/>}/>
-                    <Route path='signup' element={<Signup/>}/>
                     <Route path="*" element={<Home />} />
+                    <Route path="createevent" element={<CreateEventPage/>}/>
+                    <Route path="resetpassword" element={<ResetPasswordPage/>}/>
                 </Route>
             </Routes>
         </React.Fragment>
