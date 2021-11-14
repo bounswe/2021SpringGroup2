d5575b873208168c064ed3ab6cae3490cf5e9fce
Doğukan Akar <36790615+CenekSanzak@users.noreply.github.com>
2021-11-14 17:55:28 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,20 @@ import {
 
 import Home from './Views/Home/Index'
 import Login from './Views/Login/Index'
-import Header from './Views/Shared/Header'
+import Layout from './Views/Shared/Layout'
 import CreateEventPage from "./Views/Create Event/CreateEventPage";
-import ResetPasswordPage from "./Views/Login/ForgotPassword";
+
 
 const App = () => {
     console.log("sa")
     return (
         <React.Fragment>
             <Routes>
-                <Route path="/" element={<Header />}>
+                <Route path="/" element={<Layout />}>
                     <Route index element={<Home/>}/>
                     <Route path='login' element={<Login/>}/>
                     <Route path="*" element={<Home />} />
                     <Route path="createevent" element={<CreateEventPage/>}/>
-                    <Route path="resetpassword" element={<ResetPasswordPage/>}/>
                 </Route>
             </Routes>
         </React.Fragment>
