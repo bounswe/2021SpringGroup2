57461ee872d58980f0099337c2ba3e8d76bed153
berkaydoner <52797716+berkaydoner@users.noreply.github.com>
2021-11-16 11:59:39 +0300

    Fixed url's and request fields

--- frontend/src/Controllers/LoginController.js
+++ frontend/src/Controllers/LoginController.js
@@ -4,7 +4,7 @@ export function obtainToken(username_in, password_in){
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username:username_in,password:password_in})
     }
-    return fetch("localhost:9000/api/token/obtain",options)
+    return fetch("http://34.122.205.8/api/token/obtain",options)
         .then(response=>response.json())
         .then(r=>{console.log(r); return r})
 }
@@ -14,7 +14,7 @@ export function refreshToken(refresh_token){
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({Refresh:refresh_token})
     }
-    return fetch("https:localhost:9000/api/token/refresh",options)
+    return fetch("http://34.122.205.8/api/token/refresh",options)
         .then(response=>response.json())
         .then(r=>{console.log(r); return r})
 }
@@ -23,9 +23,9 @@ export function postResetPassword(email){
     const options = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ Email: email})
+        body: JSON.stringify({ email: email})
     }
-    return fetch("https:localhost:9000/api/password/reset",options)
+    return fetch("http://34.122.205.8/api/password/reset",options)
         .then(response=>response.json())
         .then(r=>{console.log(r); return r})
 }
@@ -33,9 +33,9 @@ export function postResetPasswordConfirmation(newPassword,token){
     const options = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ Password: newPassword, Token: token})
+        body: JSON.stringify({ password: newPassword, token: token})
     }
-    return fetch("https:localhost:9000/api/password/reset/confirm",options)
+    return fetch("http://34.122.205.8/api/password/reset/confirm",options)
         .then(response=>response.json())
         .then(r=>{console.log(r); return r})
 }
