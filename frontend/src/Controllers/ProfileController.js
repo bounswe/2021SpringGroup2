fd70fc444e984b84cdd94c0fc3300a503c8d4029
dogukanakar <dogukanakarc@gmail.com>
2021-12-07 00:51:23 +0300

    #163 addded profile controller

---
+++ frontend/src/Controllers/ProfileController.js
@@ -0,0 +1,11 @@
+export const getProfile = username =>{
+
+    const options = {
+        method: 'GET',
+        headers: { 'Accept': 'application/json','Content-Type': 'application/json'},
+    }
+    return fetch("http://34.68.66.109/api/users/"+username,options)
+        .then(response=>response.json())
+        .then(r=>{console.log(r); return r})
+}
+
