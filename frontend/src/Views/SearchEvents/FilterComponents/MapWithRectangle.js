e2ac7627fc2c07b04ed6f36dbef08e5ed3649fd3
berkaydoner <berkaydoner13@gmail.com>
2021-12-11 17:48:25 +0300

    Initialized the map with rectangular filter with two markers on bottom left and top right corners

---
+++ frontend/src/Views/SearchEvents/FilterComponents/MapWithRectangle.js
@@ -0,0 +1,136 @@
+import 'leaflet/dist/leaflet.css';
+import {MapContainer, TileLayer, Marker, CircleMarker, useMap, Rectangle} from 'react-leaflet';
+import * as React from "react";
+import {useEffect, useMemo, useRef, useState} from "react";
+import "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/images/marker-icon-2x.png";
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import {Autocomplete} from "@mui/lab";
+import {TextField} from "@mui/material";
+import {getLocationBoundaryBoxes} from "../../Controllers/GeocodingController";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from '@mui/icons-material/Search';
+
+export default function MapWithRectangle(props) {
+    let DefaultIcon = L.icon({
+        iconUrl: icon,
+        iconSize: new L.Point(15, 25),
+    });
+    L.Marker.prototype.options.icon = DefaultIcon;
+
+    const centerValue = {
+        lat:41.0082,
+        lng:28.9784
+    }
+    const [location, setLocation] = useState(null);
+    const [locationText, setLocationText] = useState("");
+    const [locations, setLocations] = useState([{}]);
+    const [optionsOpen,setOptionsOpen] = useState(false);
+    const textFieldStyle = {backgroundColor: 'white', marginTop: 10, marginBottom: 10};
+    const markerBottomLeft = useRef(null);
+    const markerTopRight = useRef(null);
+
+
+
+
+
+
+    const eventHandlersBottomLeft = useMemo(
+        () => ({
+            dragend() {
+                const marker = markerBottomLeft.current
+                if (marker != null) {
+                    let coords = marker.getLatLng()
+                    props.setBottomLeft({lat:coords.lat,lng:coords.lng})
+                }
+            },
+        }),
+        [],
+    )
+    const eventHandlersTopRight = useMemo(
+        () => ({
+            dragend() {
+                const marker = markerTopRight.current
+                if (marker != null) {
+                    let coords = marker.getLatLng()
+                    props.setTopRight({lat:coords.lat,lng:coords.lng})
+                }
+            },
+        }),
+        [],
+    )
+
+
+    function SetViewOnClick({ coords }) {
+        const map = useMap()
+        map.setView(coords, map.getZoom());
+        return null;
+    }
+    const handleLocationSearch = (event,inputQuery) => {
+        setLocationText(inputQuery)
+    }
+    const handleSearch = () => {
+
+    }
+    const selectLocation = (input) => {
+
+    }
+    return (
+        <div>
+            <Grid container spacing={1}>
+                <Grid item sm={11}>
+                    <Autocomplete
+                        open={optionsOpen}
+                        value={location}
+                        inputValue={locationText}
+                        options={locations}
+                        getOptionLabel={d=>d.name ||""}
+                        filterOptions={(x)=>x}
+                        onChange={(event, value) =>value ? selectLocation(value) : selectLocation(event.target.value)}
+                        onInputChange={handleLocationSearch}
+                        clearOnBlur={false}
+                        renderInput={params => {
+                            return (
+                                <TextField  {...params} id="location" fullWidth label="Event Location" placeholder="Search for city, district, street..." size="small" variant="outlined"
+                                            style={textFieldStyle} value={locationText|""}></TextField>
+                            )}} />
+                </Grid>
+                <Grid item sm={1}>
+                    <IconButton style={{marginTop: "10%"}} onClick={handleSearch}>
+                        <SearchIcon style={{width:"70%", height:"70%"}}/>
+                    </IconButton>
+                </Grid>
+            </Grid>
+
+
+            <div id="map">
+                <MapContainer style={{width: "100%", height: "70vh"}} center={centerValue} zoom={13}
+                              scrollWheelZoom={false} attributionControl={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker
+                        position={props.bottomLeft}
+                        draggable={"true"}
+                        ref={markerBottomLeft}
+                        eventHandlers={eventHandlersBottomLeft}>
+                        <SetViewOnClick coords={props.center}/>
+                    </Marker>
+                    <Marker
+                        position={props.topRight}
+                        draggable={"true"}
+                        ref={markerTopRight}
+                        eventHandlers={eventHandlersTopRight}>
+                        <SetViewOnClick coords={props.center}/>
+                    </Marker>
+
+
+                </MapContainer>
+            </div>
+        </div>);
+}
