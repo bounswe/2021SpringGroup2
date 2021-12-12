257e51afa7120078d4052bfb59c3835e2fd13f4f
berkaydoner <berkaydoner13@gmail.com>
2021-12-12 22:33:06 +0300

    Added unit test for fetching comment by ID

--- frontend/src/Tests/comments.test.js
+++ frontend/src/Tests/comments.test.js
@@ -50,3 +50,42 @@ describe("Check if the answers are formatted correctly given the request", () =>
         ])
     })
 })
+describe("Check if the comment is in correct format and it fetches answers", () =>{
+    let originalFetch;
+    beforeEach(() => {
+        originalFetch = global.fetch;
+        global.fetch = jest.fn(() => Promise.resolve({
+            json: () => Promise.resolve({
+                    "@context": "https://www.w3.org/ns/activitystreams",
+                    "summary": "Sally created a comment",
+                    "type": "Create",
+                    "actor": {
+                    "type": "Person",
+                        "name": "Sally"
+                    },
+                    "object": {
+                    "type": "Comment",
+                        "postId": "",
+                        "ownerId": "",
+                        "content": "A Simple Comment",
+                        "creationDate": "2014-11-31T23:00:00-08:00"
+                     }
+    })}));})
+    afterEach(() => {
+        global.fetch = originalFetch;
+    });
+    it("Check if the response is in the correct format and function calls are correct",async () => {
+        const spy = jest.spyOn(CommentAnswerController,"getAnswersOfComment")
+        const response = await CommentAnswerController.getCommentByID(11,14)
+        expect(spy).toBeCalledWith(11,14)
+        expect(spy).toHaveBeenCalledTimes(1)
+        expect(response).toEqual( {
+            user: { username: 'Sally' },
+            content: 'A Simple Comment',
+            creationDate: '2014-11-31T23:00:00-08:00',
+            isAnswer: false,
+            comment_id: 14
+        })
+        spy.mockClear()
+    })
+})
