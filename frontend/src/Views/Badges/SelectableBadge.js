e6cc9c7ebe57849d2f64a9518758373306ba1d49
berkaydoner <berkaydoner13@gmail.com>
2021-12-26 20:44:51 +0300

    Added another badge component which can be selected through click

---
+++ frontend/src/Views/Badges/SelectableBadge.js
@@ -0,0 +1,21 @@
+import {Tooltip, Typography, Stack, Grid} from "@mui/material";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+export default function SelectableBadge(props) {
+    return (
+        <Box sx={{border:3,borderColor:((props.selected===props.name)?"green":"white")}}>
+            <Tooltip title={props.description}>
+                <Grid container direction="column" alignItems="center" onClick={()=>props.setSelected(props.name)}
+                        sx={{cursor:"pointer"}}>
+                    <Grid item>
+                        <img width="50" height="50" src={`data:image/png;base64, ${props.icon}`}></img>
+                    </Grid>
+                    <Grid item>
+                        <Typography>{props.name}</Typography>
+                    </Grid>
+                </Grid>
+            </Tooltip>
+        </Box>
+    )
+}
