511748b9a20eb8c7cdcb2269a39d1d21c5ef77c9
berkaydoner <berkaydoner13@gmail.com>
2021-12-26 20:45:56 +0300

    Added a check to reset the selection when the badge is clicked twice

--- frontend/src/Views/Badges/SelectableBadge.js
+++ frontend/src/Views/Badges/SelectableBadge.js
@@ -6,7 +6,8 @@ export default function SelectableBadge(props) {
     return (
         <Box sx={{border:3,borderColor:((props.selected===props.name)?"green":"white")}}>
             <Tooltip title={props.description}>
-                <Grid container direction="column" alignItems="center" onClick={()=>props.setSelected(props.name)}
+                <Grid container direction="column" alignItems="center" onClick={()=>props.setSelected(
+                    props.selected===props.name?null:props.name)}
                         sx={{cursor:"pointer"}}>
                     <Grid item>
                         <img width="50" height="50" src={`data:image/png;base64, ${props.icon}`}></img>
