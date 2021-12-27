49eb653864600f048d45da81abd09c0274b3ea82
sseff <sefikaakman4@hotmail.com>
2021-12-28 02:53:15 +0300

    #271 Added sports from decathlon api.

--- frontend/src/Views/SearchEvents/FilterComponents/SportType.js
+++ frontend/src/Views/SearchEvents/FilterComponents/SportType.js
@@ -1,81 +1,20 @@
 import React, {useState, useEffect} from 'react'
 import Typography from "@mui/material/Typography";
-import Slider from '@mui/material/Slider';
 import {FormControl, MenuItem, Select} from "@material-ui/core";
-import {getSportsList} from '../../../Controllers/SportsController';
-import SportTypesInfoCard from "../../Home/SportTypesInfoCard";
-import Grid from "@mui/material/Grid";
-import {createStyles, makeStyles, styled} from "@mui/styles";
-
-
-const useStyles = makeStyles(theme => createStyles({
-    "@global": {
-        body: {
-            backgroundColor: theme.palette.common.white
-        }
-    },
-    paper: {
-        marginTop: theme.spacing(8),
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center"
-    },
-    fav: {
-        color: theme.palette.warning.light,
-        fontsize: 20,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center"
-    },
-    other: {
-        color: theme.palette.secondary.light,
-        fontsize: 20,
-    },
-    info: {
-        color: theme.palette.primary.light,
-        fontsize: 20,
-    },
-    infotext: {
-        color: theme.palette.secondary.light,
-        fontsize: 20,
-    },
-    others: {
-        color: theme.palette.warning.light,
-        fontsize: 20,
-    },
-    media: {
-        height: 350
-    },
-    typography: {
-        align: "center"
-    }
-
-
-}));
+import {getSports, getSportsList} from '../../../Controllers/SportsController';
 
 
 export default function SportType(props){
-    const classes = useStyles()
 
     const handleChange = (event, newValue) => {
         props.setValue(props.ids)(newValue)
     };
 
-
-    // useEffect(() => {
-    //     fetch("http://34.68.66.109/api/posts/"+eventid+"/")
-    //         .then(r=>r.json())
-    //         .then(r=>setEvent(r)||r)
-    //         .then(r=>getSportInfo(r.object.eventSport))
-    //         //.then(r=>getSportInfo("Tennis"))
-    //         .then(setSport)
-    // }, []);
-
     const [sportTypes, setSportTypes] = useState([{}])
 
-    useEffect(() => {
+    useEffect(_=>{
         getSportsList()
-            .then(setSportTypes)
+            .then(r=>setSportTypes(r))
     }, [])
 
     return(
@@ -89,16 +28,13 @@ export default function SportType(props){
                     value={props.id}
                     onChange={handleChange}
                     valueLabelDisplay="auto"
+
                 >
-                    <MenuItem value={10}>Tennis</MenuItem>
-                    <MenuItem value={20}>Football</MenuItem>
-                    <MenuItem value={30}>Basketball</MenuItem>
+                    <div>
+                        {sportTypes.map((type)=>(<MenuItem>{type.label}</MenuItem>))}
+                    </div>
                 </Select>
             </FormControl>
-            <Grid item  style={{display: 'flex'}} align={"center"}>
-                {sportTypes.map(s => <div>{s.title}</div>)}
-
-            </Grid>
 
         </React.Fragment>
     )
