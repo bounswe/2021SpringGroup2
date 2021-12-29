509f34ae806ab1ad34c28920404e29d7436cf56a
ege-kaya <egecky@gmail.com>
2021-12-29 17:45:01 +0300

    get badge by name and get all badges functions are now working

--- backend/badges/views.py
+++ backend/badges/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,6 +6,8 @@ from rest_framework import viewsets, permissions
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from badges.models import Badge
 from badges.serializers import BadgeSerializer
+from rest_framework.response import Response
+
 
 class BadgeViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
@@ -13,3 +16,39 @@ class BadgeViewSet(viewsets.ModelViewSet):
     serializer_class = BadgeSerializer
     lookup_field = 'name'
     JWTauth = JWTAuthentication()
+
+    def wrap(self, request, data):
+        response = \
+            {
+                "@context": "https://www.w3.org/ns/activitystreams",
+                "type": "Badge",
+                "name": data["name"],
+                "content": data["description"],
+                "icon":
+                    {
+                        "type": "Image",
+                        "content": data["icon"]
+                    }
+            }
+
+        return response
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(self.wrap(request, serializer.data))
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        response = []
+
+        for data in serializer.data:
+            response.append(self.wrap(request, data))
+        return Response(response)
