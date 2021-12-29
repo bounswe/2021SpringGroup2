7db4c64023f52171864ae3213fd301c6180eba2c
sseff <sefikaakman4@hotmail.com>
2021-12-29 15:44:15 +0300

    #271 Fixed sport types.

--- frontend/src/Views/SearchEvents/FilterComponents/SportType.js
+++ frontend/src/Views/SearchEvents/FilterComponents/SportType.js
@@ -2,19 +2,36 @@ import React, {useState, useEffect} from 'react'
 import Typography from "@mui/material/Typography";
 import {FormControl, MenuItem, Select} from "@material-ui/core";
 import {getSports, getSportsList} from '../../../Controllers/SportsController';
-
+import {Autocomplete} from "@mui/lab";
+import {Grid, TextField} from "@mui/material";
+import {checkIfNumber} from "../../Create Event/CreateEventPage";
+const initialState = {
+    title: {
+        value: "",
+        changed: false,
+        error: undefined
+    }
+}
 
 export default function SportType(props){
-
-    const handleChange = (event, newValue) => {
-        props.setValue(props.ids)(newValue)
-    };
-
-    const [sportTypes, setSportTypes] = useState([{}])
+    const [options, setOptions] = React.useState([{}]);
+    const [inputs, setInputs] = useState(initialState)
+    const textFieldStyle = {backgroundColor: 'white', marginTop: 10, marginBottom: 10}
+    const [sportTypes, setSportTypes] = React.useState("");
+    const getValue = inputs => ({
+        title: inputs.title.value
+    })
+    const handleChange = e=>{
+        const newInputs = {...inputs}
+        const value = getValue(newInputs)
+        value[e.target.id] = e.target.value
+        setInputs(newInputs)
+    }
 
     useEffect(_=>{
-        getSportsList()
-            .then(r=>setSportTypes(r))
+        getSportsList().then(r=>setOptions(r.sort(function (a, b) {
+            return a.label.localeCompare(b.label);})
+        )).catch(console.log)
     }, [])
 
     return(
@@ -22,19 +39,37 @@ export default function SportType(props){
             <Typography variant="body1">
                 Sport Types
             </Typography>
-            <FormControl fullWidth>
-                <Select
-                    getAriaLabel={() => 'Sport Types'}
-                    value={props.id}
-                    onChange={handleChange}
-                    valueLabelDisplay="auto"
+            <Grid item xs={12} sm={6}>
+                <Autocomplete
+                    value={sportTypes}
+                    options={options}
+                    onChange={(event, value) =>value ? setSportTypes(value.label) : setSportTypes(event.target.value)}
+                    renderInput={params => {
+                        return (
+                            <TextField
+                                {...params}
+                                variant="outlined"
+                                size="small"
+                                fullWidth
+                                required
+                                style={textFieldStyle}
+                            />
+                        )}}/>
 
-                >
-                    <div>
-                        {sportTypes.map((type)=>(<MenuItem>{type.label}</MenuItem>))}
-                    </div>
-                </Select>
-            </FormControl>
+            </Grid>
+            {/*<FormControl fullWidth>*/}
+            {/*    <Select*/}
+            {/*        getAriaLabel={() => 'Sport Types'}*/}
+            {/*        value={props[props.id]}*/}
+            {/*        onChange={handleChange}*/}
+            {/*        valueLabelDisplay="auto"*/}
+
+            {/*    >*/}
+            {/*        <div>*/}
+            {/*            {sportTypes.map((type)=>(<MenuItem>{type.label}</MenuItem>))}*/}
+            {/*        </div>*/}
+            {/*    </Select>*/}
+            {/*</FormControl>*/}
 
         </React.Fragment>
     )
