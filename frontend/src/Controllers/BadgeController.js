4e30091af9b33ca79e6bb6d89f813249e9ee8d2c
berkaydoner <berkaydoner13@gmail.com>
2021-12-29 21:19:02 +0300

    Added the controller function for fetching all badges

--- frontend/src/Controllers/BadgeController.js
+++ frontend/src/Controllers/BadgeController.js
@@ -21,3 +21,28 @@ export async function getBadgeDetails(badge_name) {
 
     return response
 }
+
+export async function getAllBadges() {
+    const options = {
+        method: 'GET',
+        headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+    }
+    let response
+    try {
+        response = await fetch("/api/badges/" , options)
+            .then(response => response.json())
+            .then((result) => {
+                return result.items.map(element=>(
+                        {
+                            name: element.object.name,
+                            icon: element.object.icon.content,
+                            description: element.object.content
+                        }
+                )
+                )})
+    } catch (err) {
+        console.log(err);
+    }
+
+    return response
+}
