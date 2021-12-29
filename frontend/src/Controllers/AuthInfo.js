4ff688c715b356442a275fc12f20912e673b5f43
sseff <sefikaakman4@hotmail.com>
2021-12-30 02:23:32 +0300

    #278 Added localstorage for authinfo.

--- frontend/src/Controllers/AuthInfo.js
+++ frontend/src/Controllers/AuthInfo.js
@@ -1,16 +1,16 @@
 const headers = {}
 
 export const setHeaders = (accessToken, refreshToken) =>{
-    headers.access = accessToken
-    headers.refresh = refreshToken
-    headers.accessTimeStamp = new Date().getTime()
+    localStorage.setItem('access', accessToken)
+    localStorage.setItem('refresh', refreshToken)
+    localStorage.setItem('accessTimeStamp', '' + new Date().getTime())
 }
 export const setUserInfo = (username,user_id) => {
-    headers.username = username
-    headers.user_id = user_id
+    localStorage.setItem('username', username)
+    localStorage.setItem('user_id', user_id)
 }
 export const getUserInfoLoggedIn = ()=>{
-    if(headers.username!==null) return ({username:headers.username,user_id:headers.user_id})
+    if(localStorage.getItem('username')!==null) return ({username:localStorage.getItem('username'),user_id:localStorage.getItem('user_id')})
     return false
 }
 export const getUserInfo = async (username) =>{
@@ -26,8 +26,8 @@ export const getUserInfo = async (username) =>{
             return r})
 }
 export const getToken = async _=>{
-    if(headers.accessTimeStamp === null) return false
-    if(new Date().getTime()-headers.accessTimeStamp < 1000*60*5) return headers.access
+    if(localStorage.getItem('accessTimeStamp') === null) return false
+    if(new Date().getTime()-parseInt(localStorage.getItem('accessTimeStamp')) < 1000*60*5) return localStorage.getItem('access')
     const options = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
