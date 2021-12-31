8f720ee6b5c13cd45b193963acfb5b37eaa00582
berkaydoner <berkaydoner13@gmail.com>
2021-12-31 15:14:30 +0300

    Added the badge count to the badge list in case the user have more than one instance of a badge

--- frontend/src/Views/Badges/BadgeList.js
+++ frontend/src/Views/Badges/BadgeList.js
@@ -1,17 +1,34 @@
 import {Grid} from "@mui/material";
 import * as React from "react";
 import Badge from "./Badge"
+import {Badge as BadgeMUI} from "@mui/material";
 export default function BadgeList(props) {
     const badgesGrid = []
-    for(let level=0;level<=props.badges.length;level+=3){
+    const counts = new Map();
+
+    for (const badge of props.badges) {
+        counts.set(badge.name, counts.get(badge.name)?counts.get(badge.name)+1:1);
+    }
+    for(let level=0;level<=counts.size;level+=3){
         badgesGrid.push(
             <Grid container spacing={3} alignItems="stretch">
-                {props.badges.slice(level,level+3).map(badge=>(
+                {
+                Array.from(counts).slice(level,level+3).map((badge)=> {
+                    let badgeName = badge[0];
+                    let badgeCount = badge[1];
+                    let badgeInfo = props.badges.find(({name}) => name === badgeName);
+                    return badgeCount === 1 ?
                         <Grid item md={4}>
-                            <Badge icon={badge.icon} name={badge.name} description={badge.description}/>
+                            <Badge icon={badgeInfo.icon} name={badgeInfo.name} description={badgeInfo.description}/>
                         </Grid>
-                    )
-                )}
+                        :
+                        <Grid item md={4}>
+                            <BadgeMUI badgeContent={badgeCount} color="primary">
+                                <Badge icon={badgeInfo.icon} name={badgeInfo.name} description={badgeInfo.description}/>
+                            </BadgeMUI>
+                        </Grid>
+                })
+                }
             </Grid>
         )
     }
