53d175c055de96f61b17ff3c7d4ec2b11407b729
dogukanakar <dogukanakarc@gmail.com>
2022-01-03 02:47:26 +0300

    fixed import bugs

--- frontend/src/Views/SearchEvents/FilterComponents/MinDate.js
+++ frontend/src/Views/SearchEvents/FilterComponents/MinDate.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Typography from "@mui/material/Typography";
 import {DatePicker} from "@mui/lab";
 import {TextField} from "@mui/material";
-import {datetoGMTString} from "./helper";
+import {datetoISOString} from "./helper";
 
 export default function MinCreationDate(props){
     return(
@@ -14,7 +14,7 @@ export default function MinCreationDate(props){
                 value={new Date(props[props.id])}
                 onChange={(newValue) => {
                     console.log(newValue.getTime())
-                    props.setValue(props.id)(datetoGMTString(newValue));
+                    props.setValue(props.id)(datetoISOString(newValue));
                     props.setValue(props.id)(newValue.toISOString());
                     props.setValue(props.id)(newValue.toISOString().split("T")[0]);
                 }}
