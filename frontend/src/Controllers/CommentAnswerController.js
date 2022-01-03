ce1996518214c688d3ac5b971897e8a83c956d6f
berkaydoner <berkaydoner13@gmail.com>
2022-01-04 00:44:59 +0300

    Separated comment API url's to posts and equipments

--- frontend/src/Controllers/CommentAnswerController.js
+++ frontend/src/Controllers/CommentAnswerController.js
@@ -1,18 +1,18 @@
 import * as CommentAnswerController from "./CommentAnswerController"
-import {getUserInfoLoggedIn} from "./AuthInfo";
+import {getToken, getUserInfoLoggedIn} from "./AuthInfo";
 
-export async function getAnswersOfComment(post_id,comment_id){
+export async function getAnswersOfComment(post_id,comment_id,isEvent){
     const options = {
         method: 'GET',
         headers: { 'Accept': 'application/json','Content-Type': 'application/json'},
     }
     let response
     try{
-        response = await fetch("/api/posts/"+String(post_id)+"/comments/"+String(comment_id)+"/",options)
+        response = await fetch("/api/"+ isEvent + "/" +String(post_id)+"/comments/"+String(comment_id)+"/answers/",options)
             .then(response=>response.json())
             .then((result)=> {
                 return result.items?result.items.map(d=>
-                        ({user:{username:d.actor.name},content:d.object.answer,
+                        ({user:{username:d.actor.name},content:d.object.content,
                             creationDate:d.object.creationDate, isAnswer:true}))
                     :[]}
             )
@@ -25,7 +25,7 @@ export async function getAnswersOfComment(post_id,comment_id){
 
 }
 
-export async function getCommentByID(post_id, comment_id) {
+export async function getCommentByID(post_id, comment_id, isEvent) {
 
     const options = {
         method: 'GET',
@@ -34,7 +34,7 @@ export async function getCommentByID(post_id, comment_id) {
 
     let comment = {}
     try{
-        await fetch("/api/posts/"+String(post_id)+"/comments/"+String(comment_id)+"/",options)
+        await fetch("/api/"+ isEvent + "/" + String(post_id)+"/comments/"+String(comment_id)+"/",options)
             .then(response => response.json())
             .then((result) => {
                     comment.user = {username: result.actor.name};
@@ -48,14 +48,13 @@ export async function getCommentByID(post_id, comment_id) {
         console.log(err);
     }
 
-    CommentAnswerController.getAnswersOfComment(post_id, comment_id).then(answerList =>
+    CommentAnswerController.getAnswersOfComment(post_id, comment_id, isEvent).then(answerList =>
         comment.answers = answerList
     )
-    console.log(comment)
     return comment
 }
 
-export async function getCommentsAndAnswersOfEvent(post_id){
+export async function getCommentsAndAnswersOfEvent(post_id, isEvent){
 
     const options = {
         method: 'GET',
@@ -63,87 +62,83 @@ export async function getCommentsAndAnswersOfEvent(post_id){
     }
     let comments = []
     try{
-        await fetch("/api/posts/"+String(post_id)+"/comments/",options)
+        await fetch("/api/"+ isEvent + "/"  +String(post_id)+"/comments/",options)
             .then(response=>response.json())
-            .then(r=>{console.log(r);return r;})
-            .then(response=>response.items.forEach(d=>
-                comments.push(CommentAnswerController.getCommentByID(Number(d.object.split("/")[3]),Number(d.object.split("/")[5])))
+            .then(response=> response.items.forEach(async d=>
+                comments.push(await CommentAnswerController.getCommentByID(post_id,d.object.id,isEvent))
             ))
     } catch(err){
         console.log(err)
     }
     return comments
 }
-export function postComment(post_id,content){
+export async function postComment(post_id, content, isEvent) {
     let username = getUserInfoLoggedIn().username
     let owner_id = getUserInfoLoggedIn().user_id
+    if (username === undefined || username === null) {
+        return null
+    }
+    let key;
+    await getToken().then(d => {
+        key = d
+    })
     const date = new Date().toISOString()
     const options = {
-        headers: { 'Accept': 'application/json','Content-Type': 'application/json'},
+        headers: {'Accept': 'application/json', 'Content-Type': 'application/json','Authorization': 'JWT ' + key},
         method: 'POST',
         body: JSON.stringify({
-            "@context": "https://www.w3.org/ns/activitystreams",
-            "summary": username + " created a comment",
-            "type": "Create",
-            "actor": {
-                "type": "Person",
-                "name": username
-            },
-            "object": {
-                "type": "Comment",
+                "name": username,
                 "postId": post_id,
                 "ownerId": owner_id,
                 "content": content,
                 "creationDate": date
             }
-        })
+        )
     }
     let comment = {}
-    comment.user={username:username};
-    comment.content=content;
-    comment.creationDate=date;
-    comment.isAnswer=false;
-    comment.answers=[];
-    fetch("/api/posts/"+String(post_id)+"/comments/",options)
-        .then(response=>response.json())
-        .then(d=> {
-            });
-    console.log("comment",comment)
+    comment.user = {username: username};
+    comment.content = content;
+    comment.creationDate = date;
+    comment.isAnswer = false;
+    comment.answers = [];
+    fetch("/api/" + isEvent + "/" + String(post_id) + "/comments/", options)
+        .then(response => response.json())
+        .then(d => {
+            comment.comment_id = d.object.id
+        });
     return comment
 }
-export function postAnswer(post_id,comment_id,content){
+export async function postAnswer(post_id, comment_id, content, isEvent) {
     let username = getUserInfoLoggedIn().username
     let owner_id = getUserInfoLoggedIn().user_id
+    if (username === undefined || username === null) {
+        return null
+    }
+    let key;
+    await getToken().then(d => {
+        key = d
+    })
     const date = new Date().toISOString()
     const options = {
-        headers: { 'Accept': 'application/json','Content-Type': 'application/json'},
+        headers: {'Accept': 'application/json', 'Content-Type': 'application/json','Authorization': 'JWT ' + key},
         method: 'POST',
         body: JSON.stringify({
-            "@context": "https://www.w3.org/ns/activitystreams",
-            "summary": username + " created an answer.",
-            "type": "Create",
-            "actor": {
-                "type": "Person",
-                "name": username
-            },
-            "object": {
-                "type": "Answer",
-                "answer": content,
-                "creationDate":  date
-            }
+            "commentId": comment_id,
+            "postId": post_id,
+            "ownerId": owner_id,
+            "content": content,
+            "creationDate": date
         })
     }
-    console.log(JSON.stringify(options.body))
     let answer = {}
-    answer.user={username:username};
-    answer.content=content;
-    answer.creationDate=date;
-    answer.isAnswer=true;
-    fetch("/api/posts/"+String(post_id)+"/comments/"+String(comment_id)+"/answers/",options)
-        .then(response=>response.json())
-        .then(d=> {
+    answer.user = {username: username};
+    answer.content = content;
+    answer.creationDate = date;
+    answer.isAnswer = true;
+    fetch("/api/" + isEvent + "/" + String(post_id) + "/comments/" + String(comment_id) + "/answers/", options)
+        .then(response => response.json())
+        .then(d => {
 
         });
-    console.log("answer",answer)
     return answer
 }
