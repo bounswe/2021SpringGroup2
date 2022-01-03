61a520829bc8b4bbf99d8a00a4c056ab760f95fe
berkaydoner <berkaydoner13@gmail.com>
2022-01-04 01:46:14 +0300

    Implemented unit test for displaying applicants

--- frontend/src/Tests/application.test.js
+++ frontend/src/Tests/application.test.js
@@ -34,3 +34,22 @@ describe("Check if user information is listed correctly", () =>{
     })
 })
 
+describe("Check applicant list fetching", () =>{
+    let originalFetch;
+    beforeEach(() => {
+        originalFetch = global.fetch;
+        global.fetch = jest.fn(() => Promise.resolve({
+            json: () => Promise.resolve({
+            "applicants":[1,2,3]
+            })}));})
+    afterEach(() => {
+        global.fetch = originalFetch;
+    });
+    it("Check if the response is in the correct format.",async () => {
+        const spy1 = jest.spyOn(ApplicationsController,"getUserListInfo")
+        const response = await ApplicationsController.getApplicationsToAnEvent(0)
+        expect(response).toHaveLength(3)
+        expect(spy1).toHaveBeenCalledWith([1,2,3])
+        expect(spy1).toHaveBeenCalledTimes(1)
+    })
+})
