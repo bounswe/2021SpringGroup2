b2eb412f52a7281eb5d86f5850c1368dcf9a107e
dogukanakar <dogukanakarc@gmail.com>
2022-01-03 16:02:15 +0300

    #280 Added search by sports link to the homepage

--- frontend/src/Views/Home/SportsInfoCard.js
+++ frontend/src/Views/Home/SportsInfoCard.js
@@ -1,9 +1,11 @@
 import {Card, CardActionArea, CardContent, CardMedia, Typography} from "@mui/material";
 import * as React from "react";
 import Button from "@mui/material/Button";
+import {useNavigate} from "react-router-dom";
 
 
 export default function SportsInfoCard(props){
+    const {navigate} = useNavigate()
     return (
         <Card sx={{ maxWidth: 345 }}  style={{ background: '#dbf0f9',justifyContent: 'center',  flexDirection: 'column'}}>
             <CardActionArea>
@@ -20,10 +22,9 @@ export default function SportsInfoCard(props){
                     <Typography align="justify" variant="body2" color="text.secondary">
                         {props.desc}
                     </Typography>
-
                 </CardContent>
             </CardActionArea>
-            <Button fullWidth style={{margin:"8px 0",padding:"auto"}}>
+            <Button onClick={_=>navigate("/search?sport="+props.title)} fullWidth style={{margin:"8px 0",padding:"auto"}}>
                 {"See " + props.title + " events"}
             </Button>
         </Card>
