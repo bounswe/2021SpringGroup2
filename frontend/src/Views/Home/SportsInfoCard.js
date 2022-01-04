42ddbd9eaab47e663146529f3f1242481052445f
dogukanakar <dogukanakarc@gmail.com>
2022-01-04 10:01:14 +0300

    made homepage useful

--- frontend/src/Views/Home/SportsInfoCard.js
+++ frontend/src/Views/Home/SportsInfoCard.js
@@ -5,7 +5,7 @@ import {useNavigate} from "react-router-dom";
 
 
 export default function SportsInfoCard(props){
-    const {navigate} = useNavigate()
+    const navigate = useNavigate()
     return (
         <Card sx={{ maxWidth: 345 }}  style={{ background: '#dbf0f9',justifyContent: 'center',  flexDirection: 'column'}}>
             <CardActionArea>
@@ -24,7 +24,10 @@ export default function SportsInfoCard(props){
                     </Typography>
                 </CardContent>
             </CardActionArea>
-            <Button onClick={_=>navigate("/search?sport="+props.title)} fullWidth style={{margin:"8px 0",padding:"auto"}}>
+            <Button onClick={_=> {
+                console.log("button clicked")
+                navigate("/search?sport=" + props.title)
+            }} fullWidth style={{margin:"8px 0",padding:"auto"}}>
                 {"See " + props.title + " events"}
             </Button>
         </Card>
